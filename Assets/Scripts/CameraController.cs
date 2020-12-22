1e4af10603e7d008919af318830ff1d2358e6f86
Antonio Estela <antonioestela73@gmail.com>
2020-12-22 15:05:16 -0500

    Fixed movement and added camera movement
    The main camera now follows the ball!

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 28, -11);
     void Update()
     {
-        
+        transform.position = player.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,19 +8,19 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     void FixedUpdate()
     {
-        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.UpArrow))
         {
             rb.AddForce(speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.DownArrow))
         {
             rb.AddForce(-speed * Time.deltaTime, 0, 0);
         }
-        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.RightArrow))
         {
             rb.AddForce(0, 0, -speed * Time.deltaTime);
         }
-        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.LeftArrow))
         {
             rb.AddForce(0, 0, speed * Time.deltaTime);
         }
