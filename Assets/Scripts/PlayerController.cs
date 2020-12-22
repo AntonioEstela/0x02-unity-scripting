d2f610a83af25a8a253b8ccd376a0a1ffd587e5e
Antonio Estela <antonioestela73@gmail.com>
2020-12-22 14:41:27 -0500

    Added task 1
    Added a new functionality (the player can move the ball using WASD or arrow keys)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 600f;
+    public Rigidbody rb;
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
